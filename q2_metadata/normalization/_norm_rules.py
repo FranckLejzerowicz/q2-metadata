e2f893e510b6a1baf5fb9e25cd9c2d721c87c5e9
FranckLejzerowicz <franck.lejzerowicz@unige.ch>
2020-04-01 13:25:57 -0700

    updated rules collection code including the format checking

--- q2_metadata/normalization/_norm_rules.py
+++ q2_metadata/normalization/_norm_rules.py
@@ -11,11 +11,74 @@ from glob import glob
 import pandas as pd
 from os.path import basename, isdir, splitext
 
-from q2_metadata.normalization._norm_messages import warningsCollection, errorsCollection
+from q2_metadata.normalization._norm_messages import WarningsCollection, ErrorsCollection
+from q2_metadata.normalization._norm_check_rules import check_rule
 
 
 class Rules(object):
-    """Collect the rules of one current variable rules.
+    """Collect the rules of one current variable.
+
+        remap must be a dict
+            Apply the rule of remapping variables values: replace
+            all instances of a given value by another one value.
+                        remap:
+                          str: str
+        validation must be a dict
+            Apply the rule of validating variables values: replace by the blank
+            (or the missing) value every sample entry that for the current variable has
+            in one or multiple other variable(s) a null or missing value for that sample.
+                        validation:
+                          force_to_blank_if:
+                            is null:
+                            - variable
+        normalization must be a dict
+            Apply the rules of normalizing the variables values, by replacing
+            the values outside the range by a given replacement value.
+                        normalization:
+                            gated_value:
+                                - Out of bounds
+                            maximum:
+                                - int
+                                - float
+                            minimum:
+                                - int
+                                - float
+        missing must be a str
+            Apply rule of checking the allowed missing values:
+            replace not-expected values and numpy's nan (empty) by
+            the passed missing value term.
+                        missing:
+                            - not applicable
+                            - not collected
+                            - not provided
+                            - restricted access
+        format must be a str
+            Apply rule of checking that the passed column in the correct format:
+            [THIS COMMAND WOULD EITHER INFER THE DTYPE (as of now) OR USE THE
+            RESULT OF AN INITIAL DTYPE GETTER FUNCTION (see _dtypes.py)]
+                        format:
+                          - bool
+                          - float
+                          - int
+                          - str
+        expected must be a list
+            Apply rule of allowing only expected values:
+            These do allow the missing and blanks.
+                        expected:
+                            - str
+                            - list
+        ontology must be a str
+            Name of the ontology to lookup within for control vocabulary, e.g.
+                        ontology: Gazetteer
+        blank must be a str
+            Apply the rules of checking the allowed blank values, by replacing
+            the not-expected values and numpy's nan (empty) by the passed
+            blank value term, e.g.
+                        blank:
+                            - not applicable
+                            - not collected
+                            - not provided
+                            - restricted access
 
     Parameters
     ----------
@@ -27,91 +90,44 @@ class Rules(object):
     variable : str
         Name of the variable which rules are parsed and
         that is the yaml file name without the .yml extension.
+    rules: dict
+        Checked rules in a hard-coded data structure that
+        has common default values for all variables and
+        that is then updated based on the parsed rules.
+    variables_rules : dict
+        Rules (values) parsed from the current variable's yml file.
+    warnings : WarningsCollection instance
+        Collection of warnings encountered during either the parsing
+        of the variables rules, or during the application the rules.
+    errors : ErrorsCollection instance
+        Collection of errors encountered during either the parsing
+        of the variables rules, or during the application the rules.
 
-    remap : dict
-        Apply the rules of remapping variables values, by replacing
-        all instances of a given value by another one value.
-                    'remap:
-                      str: str'
-    validation : dict
-        Apply the rules of validating variables values, by replacing by the blank
-        (or the missing) value every instance which in one or many other columns
-        have a null or missing value.
-                    'validation:
-                      force_to_blank_if:
-                        is null:
-                        - variable'
-    normalization : dict
-        Apply the rules of normalizing the variables values, by replacing
-        the values outside the range by a given replacement value.
-                    'normalization:
-                        gated_value:
-                            - Out of bounds
-                        maximum:
-                            - int
-                            - float
-                        minimum:
-                            - int
-                            - float'
-    missing : str
-        Apply the rules of checking the allowed missing values, by replacing
-        the not-expected values and numpy's nan (empty) by the passed
-        missing value term.
-                    'missing:
-                        - not applicable
-                        - not collected
-                        - not provided
-                        - restricted access'
-    format : str
-        Apply the rules of checking that the passed column in the correct format.
-        [THIS COMMAND WOULD EITHER INFER THE DTYPE (as of now) OR USE THE
-        RESULT OF AN INITIAL DTYPE GETTER FUNCTION (see _dtypes.py)]
-                    'format:
-                      - bool
-                      - float
-                      - int
-                      - str'
-    expected : list
-        Apply the rules of allowing only expected values.
-        These do allow the missing and blanks.
-                    'expected:
-                        - str
-                        - list'
-    ontology : str
-        Name of the ontology to lookup within for control vocabulary, e.g.
-                    'ontology: Gazetteer'
-    blank : str
-        Apply the rules of checking the allowed blank values, by replacing
-        the not-expected values and numpy's nan (empty) by the passed
-        blank value term, e.g.
-                    'blank:
-                        - not applicable
-                        - not collected
-                        - not provided
-                        - restricted access'
+    Methods
+    -------
+    parse_rule : staticmethod
+        Performs the parsing of the current variable's yml file and
+        returns the rules (values) in `self.variables_rules`.
+    _check_variable_rules
+        Loops over each rule and check that they are well formatted
+        and either raise a specific issue error or a fill the rules dict.
+    normalize
+        Perform the application of the variables' rules.
 
-    Raises
-    ------
-    ValueError
-        # If the remote argument is not of ``bool`` or ``str`` type.
-        # If none of the samples in the ordination matrix are in the metadata.
-        # If the data is one-dimensional.
-    KeyError
-        # If there's samples in the ordination matrix but not in the metadata.
     """
     def __init__(self, variable_rules_fp: str):
         """
-        Initialize the class instance for one set of rules beloning
-        to one single metadata variable with it possible attribute, as
-        a rate of one attribute per possible rule, plus the current
-        variable name.
+        Initialize the class instance for the set of rules associated
+        with a single metadata variable. The instantiated class will be
+        given a `variable` attribute corresponding to the variable name
+        as string, as well as a `rules` attribute collecting the actual
+        rules in a dictionary.
 
         Parameters
         ----------
         variable_rules_fp : str
             Path to one variable's rules yaml file.
         """
-        self.variable = basename(variable_rules_fp).split('.yml')[0]
         self.rules = {
             'edits': {
                 'remap': {},
@@ -128,9 +144,14 @@ class Rules(object):
             },
             'format': None
         }
-        self._process_rule(variable_rules_fp)
+        self.variable = splitext(basename(variable_rules_fp))[0]
+        self.variables_rules = self.parse_rule(variable_rules_fp)
+        self.warnings = WarningsCollection()
+        self.errors = ErrorsCollection()
+        self._check_variable_rules()
 
-    def _process_rule(self, variable_rules_fp: str):
+    @staticmethod
+    def parse_rule(variable_rules_fp: str):
         """Read the yaml file of the current variable's rules.
 
         Parameters
@@ -140,87 +161,24 @@ class Rules(object):
 
         Returns
         -------
-
+        yaml_loaded : dict
+            Parsed variable's rules.
         """
         with open(variable_rules_fp) as handle:
-            variable_rules = yaml.load(handle, Loader=yaml.FullLoader)
-            self._check_variable_rules(variable_rules)
+            try:
+                yaml_loaded = yaml.load(handle, Loader=yaml.FullLoader)
+                return yaml_loaded
+            except yaml.YAMLError:
+                raise IOError("Something is wrong with the .yml rule file.")
 
-    def _check_variable_rules(self, variable_rules: dict):
+    def _check_variable_rules(self):
         """This functions adds encountered issues
         to the warning/error class objects.
-
-        Parameters
-        ----------
-        variable_rules : dict
-
         """
-        for rule, rule_value in variable_rules.items():
-            if rule == 'expected':
-                if isinstance(rule_value, list):
-                    str_items = [x for x in rule_value if isinstance(x, str)]
-                    if len(str_items) == len(rule_value):
-                        self.rules['allowed'][rule] = rule_value
-                    else:
-                        errorsCollection(self.variable, rule, rule_value)
+        for rule, rule_value in self.variables_rules.items():
+            check_rule(self.variable, self.rules, rule, rule_value)
+        print(self.rules)
 
-            elif rule == 'ontology':
-                if isinstance(rule_value, str):
-                    if rule_value in ['Gazetteer']:
-                        self.rules['lookups'][rule] = rule_value
-
-            elif rule == 'remap':
-                if isinstance(rule, dict):
-                    str_values = [x for x,y in rule_value.items() if isinstance(y, str)]
-                    if len(str_values) == len(rule_value.keys()):
-                        self.rules['edits'][rule] = rule_value
-                    else:
-                        errorsCollection(self.variable, rule, rule_value)
-
-            elif rule == 'validation':
-                possible_validations = {
-                    'force_to_blank_if': {'is_null'}
-                }
-                if isinstance(rule_value, dict):
-                    rule_value_set = set(rule_value)
-                    if len(rule_value_set ^ (rule_value_set & set(possible_validations))):
-                        errorsCollection(self.variable, rule, rule_value)
-                        # raise and stop ? or keep checking?
-                    else:
-                        for rule_2, rule_value_2 in rule_value['force_to_blank_if'].items():
-                            if isinstance(rule_value_2, list):
-                                possible_validations_2 = possible_validations['force_to_blank_if']
-                                if len(set(rule_2) ^ (set(rule_2) & possible_validations_2)):
-                                    errorsCollection(self.variable, rule, rule_value)
-                                else:
-                                    self.rules['edits'][rule] = rule_value
-                            else:
-                                errorsCollection(self.variable, rule, rule_value)
-                else:
-                    errorsCollection(self.variable, rule, rule_value)
-
-            elif rule == 'normalization':
-                if isinstance(rule_value, dict):
-                    possible_normalizations = {'maximum', 'mininum', 'gated_value'}
-                    rule_value_set = set(rule_value)
-                    if len(rule_value_set ^ (rule_value_set & possible_normalizations)):
-                        errorsCollection(self.variable, rule, rule_value)
-                    else:
-                        self.rules['edits'][rule] = rule_value
-
-            elif rule == 'blank':
-                if isinstance(rule_value, str):
-                    self.rules['allowed'][rule] = rule_value
-
-            elif rule == 'missing':
-                if isinstance(rule_value, str):
-                    self.rules['allowed'][rule] = rule_value
-
-            elif rule == 'format':
-                if isinstance(rule_value, str):
-                    self.rules['format'][rule] = rule_value
-
-    # methods to apply the rules onto
     def normalize(self, variable: str, input_column: pd.Series) -> pd.Series:
         """
         Dispatch the metadata column to be curated to
@@ -241,6 +199,7 @@ class Rules(object):
         -------
         output_column : pd.Series
             Curated (or being curated) metadata column.
+
         """
         pass
 
@@ -261,26 +220,38 @@ class RulesCollection(object):
         and decide whether to bother users with failed rules parsing
         checks for rules that are irrelevant to this user, i.e. shall
         we only parse rules in the focus?)
-    warnings : warningsCollection instance
+    warnings : WarningsCollection instance
         Collection of warnings encountered during either the parsing
         of the variables rules, or during the application the rules.
-    errors : errorsCollection instance
+    errors : ErrorsCollection instance
         Collection of errors encountered during either the parsing
         of the variables rules, or during the application the rules.
+
+    Methods
+    -------
+    _check_variables_rules_dir
+        Performs checks on rules paths input. Either raise Exceptions,
+        or return the list `variables_rules_files` containing the paths
+        to the existing yml rules files.
+    parse_variables_rules
+        Parse the yml rules files, one variable at a time and returns the
+        dict `variables_rules` that for each variable (keys) leads to an
+        instance of the `Rules()` class. This class performs rule-formatting
+        checks and fills a rules dict structure.
+
+    Raises
+    ------
+    IOError
+        If directory passed to --p-rules-dir does not exist.
+    FileNotFoundError
+        If directory passed to --p-rules-dir contains no .yml file.
+
     """
 
     def __init__(self, variables_rules_dir):
+        self.warnings = WarningsCollection()
+        self.errors = ErrorsCollection()
         self.variables_rules_fps = self._check_variables_rules_dir(variables_rules_dir)
-
-        # initialize the collection objects for warning and errors
-        self.warnings = warningsCollection()
-        self.errors = errorsCollection()
-
-        # parse all the variables' yaml rules files
-        self.variables_rules = self.parse_variables_rules()
-
-    def get_variables_names(self):
-        return sorted(self.variables_rules.keys())
 
     def parse_variables_rules(self) -> dict:
         """
@@ -290,6 +261,7 @@ class RulesCollection(object):
         -------
         variables_rules : dict
             Rules (values) for each metadata variable (keys).
+
         """
         variables_rules = {}
         for variable_rules_fp in self.variables_rules_fps:
@@ -299,7 +271,8 @@ class RulesCollection(object):
             variables_rules[variable] = Rules(variable_rules_fp)
         return variables_rules
 
-    def _check_variables_rules_dir(self, variables_rules_dir: str) -> list:
+    @staticmethod
+    def _check_variables_rules_dir(variables_rules_dir: str) -> list:
         """Checks that the yaml rules directory exists
         and that it contains yml files. If yes, returns
         the files in a list.
@@ -313,6 +286,7 @@ class RulesCollection(object):
         -------
         variables_rules_files : list
             Paths to the yaml rules files.
+
         """
         if not isdir(variables_rules_dir):
             raise IOError("Input directory %s does not exist" % variables_rules_dir)
